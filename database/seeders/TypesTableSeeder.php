33175e17664dfb587c66419d4162455dc32dd65d
gaetano-94 <gaetano.panico1994@gmail.com>
2024-02-11 11:02:00 +0100

    feat: seeder for model type

---
+++ database/seeders/TypesTableSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Type;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+
+class TypesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $types = ['Frontend', 'Backend', 'AI', 'Data Analytics'];
+
+        foreach ($types as $type) {
+
+            $new_type = new Type();
+
+            $new_type->title = $type;
+            $new_type->slug =
+                Str::of($type)->slug('-');
+
+            $new_type->save();
+        }
+    }
+}
